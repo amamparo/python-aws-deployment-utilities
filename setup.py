d3d27023e5d875630244de4ee76aa56ed5d4d1fd
bumpversion <bumpversion@circleci.com>
2020-10-04 03:33:35 +0000

    "Bump version: b'0.0.1\n' → 0.0.2 [skip ci]"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='aws-deployment-utilities',
-    version='0.0.1',
+    version='0.0.2',
     author='Aaron Mamparo',
     author_email='aaronmamparo@gmail.com',
     description='Helper functions and classes for common AWS operations during deployment',

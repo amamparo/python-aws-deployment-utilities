123414178d6be10ba2d814fd3fa92143b7b32686
Aaron Mamparo <aaronmamparo@gmail.com>
2020-10-03 22:25:54 -0500

    adding get_secret.py

--- src/aws_deployment_utilities/__init__.py
+++ src/aws_deployment_utilities/__init__.py
@@ -1,7 +1,9 @@
 from .create_lambda_function import create_lambda_function
 from .get_stack_output import get_stack_output
+from .get_secret import get_secret
 
 __all__ = [
     create_lambda_function.__name__,
-    get_stack_output.__name__
+    get_stack_output.__name__,
+    get_secret.__name__
 ]
